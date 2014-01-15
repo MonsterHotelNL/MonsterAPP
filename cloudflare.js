926e9f5cc92f36693b1708009f6c0051b5901898
MonsterHotel <monsterhotelnl@gmail.com>
2014-01-15 02:08:26 +0100

    Create cloudflare.js

---
+++ cloudflare.js
@@ -0,0 +1,13 @@
+/**
+ *
+ *      CloudFlareJS Hello World
+ *
+ */
+
+CloudFlare.require(
+    ['cloudflare/console'],
+    function(console) {
+
+        console.log("Hello, CloudFlare!");
+    }
+);
